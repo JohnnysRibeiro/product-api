a1a0ba6a74af32dd093b6d0301a214430ae9d7a2
JohnnysRibeiro <johnnys.ribeiro@gmail.com>
2018-04-08 18:51:12 -0300

    Changing from sqlite3 to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180408140817) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
